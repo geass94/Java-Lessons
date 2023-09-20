426c4eac2d3d12d384befae528abf5c6dca1ffd8
Otto Koridze <okoridze@yahoo.com>
2023-09-20 21:13:02 +0400

    WIP: Lesson 2

--- src/main/java/ge/itestep/Lesson_Two/Main.java
+++ src/main/java/ge/itestep/Lesson_Two/Main.java
@@ -18,9 +18,9 @@ public class Main {
 
         Set<String> stringSet = new HashSet<>();
 
-        HashMap<String, HashMap<String, String>>  studentScore = new HashMap<>();
+        HashMap<String, HashMap<String, String>> studentScore = new HashMap<>();
 
-        HashMap<String, String > cities = new HashMap<>();
+        HashMap<String, String> cities = new HashMap<>();
         cities.put("georgia", "tbilisi");
         cities.put("germany", "berlin");
 
@@ -28,6 +28,65 @@ public class Main {
 
         HashMap score = studentScore.get("2023-09-10");
 
-        System.out.println(score);
+        TreeSet<String> treeList = new TreeSet<>();
+
+        treeList.add("ვაშლი");
+        treeList.add("ალუბალი");
+        treeList.add("ბანანი");
+//
+//        System.out.println(treeList);
+//
+//        System.out.println(score);
+
+        LinkedList<String> linkedList = new LinkedList<>();
+
+        linkedList.add("ნიკა");
+        linkedList.add("სანდრო");
+        linkedList.add("ელენე");
+        linkedList.add("გიორგი");
+        linkedList.add("ირაკლი");
+        linkedList.add("საბა");
+
+        linkedList.removeFirst();
+        linkedList.removeLast();
+        linkedList.addFirst("ოთო");
+        linkedList.addAll(new ArrayList<>(){{
+            add("გელა");
+            add("ზურა");
+        }});
+
+//        System.out.println(linkedList);
+
+
+        Collections.sort(stringList, (s1, s2) -> s1.compareTo(s2));
+        System.out.println(stringList);
+
+
+        List<Integer> numbers = new ArrayList<>(){{
+            add(1);
+            add(2);
+            add(3);
+            add(4);
+            add(5);
+            add(6);
+            add(7);
+            add(8);
+            add(9);
+            add(10);
+        }};
+//        System.out.println(numbers);
+//        List<Integer> odds = numbers.stream().filter(n -> n % 2 == 0).collect(Collectors.toList());
+        List<Integer> odds = new ArrayList<>();
+        for(Integer n : numbers) {
+            if (n % 2 == 0) {
+                odds.add(n);
+            }
+        }
+
+        CustomAnnotation anotation = Student.class.getAnnotation(CustomAnnotation.class);
+        if (anotation != null) {
+            System.out.println(anotation.age());
+        }
     }
 }
+
---
+++ src/main/java/ge/itestep/Lesson_Two/Student.java
@@ -0,0 +1,7 @@
+package ge.itestep.Lesson_Two;
+
+@CustomAnnotation(firstName = "My name is Otto", age = 30)
+public class Student {
+
+}
+

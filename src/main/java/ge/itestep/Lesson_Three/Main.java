4e556ebc2eb038cfb93dc7b747935b303d804c1f
Otto Koridze <okoridze@yahoo.com>
2023-09-22 21:50:28 +0400

    WIP: Lesson 3

--- src/main/java/ge/itestep/Lesson_Three/Main.java
+++ src/main/java/ge/itestep/Lesson_Three/Main.java
@@ -51,56 +51,110 @@ public class Main {
 //            throw new RuntimeException(e);
 //        }
 
+//        try {
+//            PipedInputStream pis = new PipedInputStream();
+//            PipedOutputStream pos = new PipedOutputStream();
+//            pis.connect(pos); // აკავშირებს Input-ს Output-თან
+//
+//            Thread producer = new Thread(() -> {
+//                try {
+//                    for (int i = 0; i < 5; i++) {
+//                        pos.write(("Hello this is me from other thread: " + i + "\n").getBytes());
+//                        Thread.sleep(1000); // აფერხებს კოდის შესრულებას ერთი წაამით
+//                    }
+//                    pos.close();
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            });
+//
+//            Thread listener = new Thread(() -> {
+//                try {
+//                    int input;
+//
+//                    while((input = pis.read()) != -1) {
+//                        ArrayList<Integer> chars = new ArrayList<>();
+//                        chars.add(input);
+//                        String str = chars
+//                                .stream()
+//                                .map(s -> (char)s.intValue())
+//                                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append).toString();
+//                        System.out.print(str);
+//                    }
+//                    pis.close();
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            });
+//
+////            ვუშვებთ სრედებს
+//            producer.start();
+//            listener.start();
+//
+////            უცდის სრედების ოპერაციის შესრულებას
+//            producer.join();
+//            listener.join();
+//
+//        } catch (IOException | InterruptedException e) { // შეგვილია ველოდოთ რამდენიმე ტიპის ექსეფშენს
+//            e.printStackTrace();
+//        }
+
+//        try {
+//            FileInputStream fis = new FileInputStream("input.txt");
+//
+//            BufferedInputStream bufferedInputStream = new BufferedInputStream(fis);
+//
+//            int input;
+//            while((input = bufferedInputStream.read()) != -1) {
+//                System.out.print((char) input);
+//            }
+//            bufferedInputStream.close();
+//            fis.close();
+//
+//            FileOutputStream fos = new FileOutputStream("output.txt");
+//            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fos);
+//
+//
+//            String message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus rhoncus quam quis mi maximus rhoncus. Mauris nec odio eu sapien iaculis dignissim a ac neque. In ultricies commodo nisi, in pharetra nulla pulvinar in. Nam scelerisque nisi sed purus blandit vestibulum. Phasellus ultricies risus dolor, a finibus elit lacinia sit amet. Integer id tincidunt turpis. Ut euismod imperdiet diam eget convallis. Vivamus a iaculis nulla. Morbi quis faucibus arcu. In aliquet euismod viverra. Morbi quis ornare risus. Sed vel augue eros. Integer tempus ligula eu libero rutrum, a maximus nibh dictum.";
+//            byte[] bytes = message.getBytes();
+//            bufferedOutputStream.write(bytes);
+//            bufferedOutputStream.close();
+//            fos.close();
+//
+//            fis = new FileInputStream("output.txt");
+//            fos = new FileOutputStream("output-copy.txt");
+//            int data;
+//            while((data = fis.read()) != -1) {
+//                fos.write(data);
+//            }
+//
+//            fis.close();
+//            fos.close();
+//            System.out.println("File copied");
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+
         try {
-            PipedInputStream pis = new PipedInputStream();
-            PipedOutputStream pos = new PipedOutputStream();
-            pis.connect(pos); // აკავშირებს Input-ს Output-თან
+            Student st1 = new Student();
+            st1.setFirstName("Otto");
+            st1.setLastName("Koridze");
+            FileOutputStream fos = new FileOutputStream("student.txt");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(st1);
 
-            Thread producer = new Thread(() -> {
-                try {
-                    for (int i = 0; i < 5; i++) {
-                        pos.write(("Hello this is me from other thread: " + i + "\n").getBytes());
-                        Thread.sleep(1000); // აფერხებს კოდის შესრულებას ერთი წაამით
-                    }
-                    pos.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            });
 
-            Thread listener = new Thread(() -> {
-                try {
-                    int input;
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream("student.txt"));
 
-                    while((input = pis.read()) != -1) {
-                        ArrayList<Integer> chars = new ArrayList<>();
-                        chars.add(input);
-                        String str = chars
-                                .stream()
-                                .map(s -> (char)s.intValue())
-                                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append).toString();
-                        System.out.print(str);
-                    }
+            Student st2 = (Student) ois.readObject();
 
-                    pis.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            });
+            System.out.println(st2.getLastName());
 
-//            ვუშვებთ სრედებს
-            producer.start();
-            listener.start();
 
-//            უცდის სრედების ოპერაციის შესრულებას
-            producer.join();
-            listener.join();
-
-        } catch (IOException | InterruptedException e) { // შეგვილია ველოდოთ რამდენიმე ტიპის ექსეფშენს
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
-
 
     }
 }
---
+++ src/main/java/ge/itestep/Lesson_Three/Student.java
@@ -0,0 +1,35 @@
+package ge.itestep.Lesson_Three;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private String firstName;
+    private String lastName;
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+
+}

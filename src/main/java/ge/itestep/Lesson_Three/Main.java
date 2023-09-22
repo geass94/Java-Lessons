2a77df66efad1df4a3eb72150d50a54a1cc8e29c
Otto Koridze <okoridze@yahoo.com>
2023-09-22 21:25:45 +0400

    WIP: Lesson 3

--- src/main/java/ge/itestep/Lesson_Three/Main.java
+++ src/main/java/ge/itestep/Lesson_Three/Main.java
@@ -1,6 +1,7 @@
 package ge.itestep.Lesson_Three;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.zip.InflaterInputStream;
@@ -50,46 +51,54 @@ public class Main {
 //            throw new RuntimeException(e);
 //        }
 
-//        try {
-//            PipedInputStream pis = new PipedInputStream();
-//            PipedOutputStream pos = new PipedOutputStream();
-//            pis.connect(pos); // აკავშირებს Input-ს Output-თან
-//
-//            Thread producer = new Thread(() -> {
-//                try {
-//                    for (int i = 0; i < 5; i++) {
-//                        pos.write(("Hello this is me from other thread: " + i).getBytes());
-//                        Thread.sleep(1000); // აფერხებს კოდის შესრულებას ერთი წაამით
-//                    }
-//                    pos.close();
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//            });
-//
-//            Thread listener = new Thread(() -> {
-//                try {
-//                    int input;
-//                    while((input = pis.read()) != -1) {
-//                        System.out.println((char)input);
-//                    }
-//                    pis.close();
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//            });
-//
-////            ვუშვებთ სრედებს
-//            producer.start();
-//            listener.start();
-//
-////            უცდის სრედების ოპერაციის შესრულებას
-//            producer.join();
-//            listener.join();
-//
-//        } catch (IOException | InterruptedException e) { // შეგვილია ველოდოთ რამდენიმე ტიპის ექსეფშენს
-//            e.printStackTrace();
-//        }
+        try {
+            PipedInputStream pis = new PipedInputStream();
+            PipedOutputStream pos = new PipedOutputStream();
+            pis.connect(pos); // აკავშირებს Input-ს Output-თან
+
+            Thread producer = new Thread(() -> {
+                try {
+                    for (int i = 0; i < 5; i++) {
+                        pos.write(("Hello this is me from other thread: " + i + "\n").getBytes());
+                        Thread.sleep(1000); // აფერხებს კოდის შესრულებას ერთი წაამით
+                    }
+                    pos.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+
+            Thread listener = new Thread(() -> {
+                try {
+                    int input;
+
+                    while((input = pis.read()) != -1) {
+                        ArrayList<Integer> chars = new ArrayList<>();
+                        chars.add(input);
+                        String str = chars
+                                .stream()
+                                .map(s -> (char)s.intValue())
+                                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append).toString();
+                        System.out.print(str);
+                    }
+
+                    pis.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+
+//            ვუშვებთ სრედებს
+            producer.start();
+            listener.start();
+
+//            უცდის სრედების ოპერაციის შესრულებას
+            producer.join();
+            listener.join();
+
+        } catch (IOException | InterruptedException e) { // შეგვილია ველოდოთ რამდენიმე ტიპის ექსეფშენს
+            e.printStackTrace();
+        }
 
 
 

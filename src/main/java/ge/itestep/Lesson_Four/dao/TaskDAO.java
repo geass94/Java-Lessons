dcecd951ea7b4922642ca6785d58dfe7f0067c4f
Otto Koridze <okoridze@yahoo.com>
2023-09-25 20:54:00 +0400

    WIP: Lesson 4

--- src/main/java/ge/itestep/Lesson_Four/dao/TaskDAO.java
+++ src/main/java/ge/itestep/Lesson_Four/dao/TaskDAO.java
@@ -1,34 +1,38 @@
 package ge.itestep.Lesson_Four.dao;
 
+import ge.itestep.Lesson_Four.DatabaseConnection;
 import ge.itestep.Lesson_Four.Task;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TaskDAO {
 
-    public int createTask(Task taask)
-    {
-        return 1;
+    public int createTask(Task taask) {
+        try (Connection conn = DatabaseConnection.getConnection()) {
+            String insertQuery = "INSERT INTO tasks";
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1;
     }
 
-    public Task getById(int id)
-    {
+    public Task getById(int id) {
         return new Task();
     }
 
-    public List<Task> getAll()
-    {
+    public List<Task> getAll() {
         return new ArrayList<>();
     }
 
-    public Task update(Task task, int id)
-    {
+    public Task update(Task task, int id) {
         return new Task();
     }
 
-    public boolean delete(int id)
-    {
+    public boolean delete(int id) {
         return true;
     }
 

97e80e3becea50d80db77b0978a3af62e53ea962
Otto Koridze <okoridze@yahoo.com>
2023-09-27 21:57:03 +0400

    EOL: Lesson 5

--- src/main/java/ge/itestep/Lesson_Five/TodoServlet.java
+++ src/main/java/ge/itestep/Lesson_Five/TodoServlet.java
@@ -1,4 +1,7 @@
 package ge.itestep.Lesson_Five;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ge.itestep.Lesson_Four.Task;
+import ge.itestep.Lesson_Four.dao.TaskDAO;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -7,11 +10,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/api/tasks")
+@WebServlet("/api/todo")
 public class TodoServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("This is working");
+
+
+        TaskDAO dao = new TaskDAO();
+
+        Task task = dao.getById(4);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        String json = objectMapper.writeValueAsString(task);
+
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        resp.getWriter().write(json);
     }
 }

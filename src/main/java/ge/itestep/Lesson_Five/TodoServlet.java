1072978aec124abfa050b247f7247d514448d81f
Otto Koridze <okoridze@yahoo.com>
2023-09-27 20:24:01 +0400

    WIP: Lesson 5

---
+++ src/main/java/ge/itestep/Lesson_Five/TodoServlet.java
@@ -0,0 +1,17 @@
+package ge.itestep.Lesson_Five;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/api/tasks")
+public class TodoServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("This is working");
+    }
+}

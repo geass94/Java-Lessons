2733acbfce93600987ca648d90b652c8af715abd
WYBM <saluqvadze2006@gmail.com>
2023-10-06 21:53:51 +0400

    cw errorhandling

--- src/main/java/ge/itestep/Lesson_Nine/Contact.java
+++ src/main/java/ge/itestep/Lesson_Nine/Contact.java
@@ -1,6 +1,13 @@
 package ge.itestep.Lesson_Nine;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Contact {
+    //regex
+    private static final String regexPattern = "^\\+995\\s(559|557|555|599|593|...)\\s\\d{6}$";
+
+
     private String name;
     private String phoneNumber;
 
@@ -17,7 +24,12 @@ public class Contact {
         return phoneNumber;
     }
 
-    public static Contact createContact(String name, String phoneNumber) {
+    public static Contact createContact(String name, String phoneNumber) throws Exception {
+        Pattern pattern = Pattern.compile(regexPattern);
+        Matcher matcher = pattern.matcher(phoneNumber);
+        if (!matcher.matches()){
+            throw new Exception("This PhoneNumber - " +phoneNumber + "Is in invalid format, or wrong. (+995 559 123456)");
+        }
         return new Contact(name, phoneNumber);
     }
 }
--- src/main/java/ge/itestep/Lesson_Nine/MobilePhone.java
+++ src/main/java/ge/itestep/Lesson_Nine/MobilePhone.java
@@ -3,6 +3,27 @@ package ge.itestep.Lesson_Nine;
 import java.util.ArrayList;
 
 public class MobilePhone {
+
+
+    public static void main(String[] args) {
+        MobilePhone mp = new MobilePhone("599 99 99 99");
+
+        try{
+            mp.addNewContact(Contact.createContact("vigaca1","+995 599 234567"));
+            mp.addNewContact(Contact.createContact("vigaca2 gamsxmeli","1234567"));
+            mp.addNewContact(Contact.createContact("vigaca3","+995 32 1234567"));
+            mp.addNewContact(Contact.createContact("vigaca4","+995 32 1234567"));
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+
+
+
+
+    }
+
+
     private String myNumber;
     private ArrayList<Contact> myContacts;
 
@@ -24,7 +45,10 @@ public class MobilePhone {
         if (position >= 0) {
             myContacts.set(position, newContact);
             return true;
+        }else{
+            System.out.println("Couldn't find contact");
         }
+
         return false;
     }
 
@@ -33,6 +57,8 @@ public class MobilePhone {
         if (position >= 0) {
             myContacts.remove(position);
             return true;
+        }else{
+            System.out.println("Couldn't find contact");
         }
         return false;
     }
@@ -41,10 +67,10 @@ public class MobilePhone {
         return myContacts.indexOf(contact);
     }
 
-    public int findContact(String name) {
+    public int findContact(String keyword) {
         for (int i = 0; i < myContacts.size(); i++) {
             Contact contact = myContacts.get(i);
-            if (contact.getName().equals(name)) {
+            if (contact.getName().equals(keyword) || contact.getPhoneNumber().equals(keyword)) {
                 return i;
             }
         }
@@ -59,6 +85,8 @@ public class MobilePhone {
         return null;
     }
 
+
+
     public void printContacts() {
         System.out.println("Contact List:");
         for (int i = 0; i < myContacts.size(); i++) {
@@ -66,5 +94,7 @@ public class MobilePhone {
             System.out.println((i + 1) + ". " + contact.getName() + " -> " + contact.getPhoneNumber());
         }
     }
+
+
 }
 

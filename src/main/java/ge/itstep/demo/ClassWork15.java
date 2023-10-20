8da70b3ff83899ca67ae622b2b66fbd665f3fb67
Otto Koridze <okoridze@yahoo.com>
2023-10-20 21:05:22 +0400

    WIP: Lesson 15

---
+++ src/main/java/ge/itstep/demo/ClassWork15.java
@@ -0,0 +1,23 @@
+package ge.itstep.demo;
+
+public class ClassWork15 {
+    public static void main(String[] args) {
+
+        Node root = new Node(10);
+        root.left = new Node(5);
+        root.right = new Node(15);
+        root.left.left = new Node(3);
+        root.left.right = new Node(7);
+        root.right.left = new Node(12);
+        root.right.right = new Node(28);
+
+        Node result = root.find(12);
+
+    }
+
+    class Node {
+        int value;
+        Node left;
+        Node right;
+    }
+}

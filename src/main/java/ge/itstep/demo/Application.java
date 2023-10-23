aa4ffc813b7f261db0ecbf8c2a5dd1288c11e429
WYBM <saluqvadze2006@gmail.com>
2023-10-23 21:44:58 +0400

    CW shipformula

--- src/main/java/ge/itstep/demo/Application.java
+++ src/main/java/ge/itstep/demo/Application.java
@@ -6,11 +6,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-@EnableAutoConfiguration
+//@SpringBootApplication
+//@EnableAutoConfiguration
 public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+//		SpringApplication.run(Application.class, args);
+
+
+
+
+
+
 	}
 
 	/*

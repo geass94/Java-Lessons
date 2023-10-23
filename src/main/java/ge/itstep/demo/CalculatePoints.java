994c9d7de3d5744d73f5848d2a12666abc943d6b
Otto Koridze <okoridze@yahoo.com>
2023-10-23 20:32:00 +0400

    WIP: Lesson 16

---
+++ src/main/java/ge/itstep/demo/CalculatePoints.java
@@ -0,0 +1,108 @@
+package ge.itstep.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CalculatePoints {
+
+    public static void main(String[] args) {
+
+        final int gridSize = 10;
+
+        List<Ship> ships = new ArrayList<>();
+
+
+        Ship ship1 = new Ship(2,2, 5, 2);
+
+//                          [X][Y]
+        boolean occupiedCells[][] = new boolean[gridSize][gridSize];
+
+        if (ship1.isHorizontal()) {
+            for (int x = ship1.getX1(); x <= ship1.getX2(); x++) {
+
+                occupiedCells[x][ship1.getY1()] = true;
+
+                if (ship1.getX1() > 0) {
+                    occupiedCells[x - 1][ship1.getY1()] = true;
+                }
+                if (ship1.getX2() < gridSize) {
+                    occupiedCells[x + 1][ship1.getY1()] = true;
+                }
+                if (ship1.getY1() > 0) {
+                    occupiedCells[x][ship1.getY1() - 1] = true;
+                }
+                if (ship1.getY2() < gridSize) {
+                    occupiedCells[x][ship1.getY2() + 1] = true;
+                }
+
+                if (ship1.getX1() > 0 &&
+                        ship1.getY1() > 0 &&
+                        ship1.getX2() < gridSize &&
+                        ship1.getY2() < gridSize
+                ) {
+                    occupiedCells[x - 1][ship1.getY1() - 1] = true;
+                    occupiedCells[x - 1][ship1.getY1() + 1] = true;
+                    occupiedCells[x + 1][ship1.getY2() - 1] = true;
+                    occupiedCells[x + 1][ship1.getY2() - 1] = true;
+                }
+
+            }
+        }
+
+    }
+
+}
+
+class Ship {
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
+
+    public Ship() {
+    }
+
+    public Ship(int x1, int y1, int endX, int y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = endX;
+        this.y2 = y2;
+    }
+
+    public boolean isHorizontal()
+    {
+        return y1 == y2;
+    }
+
+    public int getX1() {
+        return x1;
+    }
+
+    public void setX1(int x1) {
+        this.x1 = x1;
+    }
+
+    public int getY1() {
+        return y1;
+    }
+
+    public void setY1(int y1) {
+        this.y1 = y1;
+    }
+
+    public int getX2() {
+        return x2;
+    }
+
+    public void setX2(int x2) {
+        this.x2 = x2;
+    }
+
+    public int getY2() {
+        return y2;
+    }
+
+    public void setY2(int y2) {
+        this.y2 = y2;
+    }
+}
--- src/main/java/ge/itstep/demo/ClassWork15.java
+++ src/main/java/ge/itstep/demo/ClassWork15.java
@@ -4,15 +4,19 @@ public class ClassWork15 {
     public static void main(String[] args) {
 
         Node root = new Node(10);
+
         root.left = new Node(5);
         root.right = new Node(15);
+
+
         root.left.left = new Node(3);
         root.left.right = new Node(7);
+
         root.right.left = new Node(12);
         root.right.right = new Node(28);
 
-        Node result = root.find(12);
-
+        Node result = root.find(15);
+        System.out.println(result.toString());
     }
 
 
@@ -25,11 +29,33 @@ class Node {
 
     public Node (int v)
     {
-
+        this.value = v;
+        this.left = null;
+        this.right = null;
     }
 
-    public Node find(int v)
-    {
-        return this;
+    public Node find(int v) {
+        Node current = this;
+
+        while (current != null) {
+            if (v == current.value) {
+                return current; // Found the node with the value
+            } else if (v < current.value) {
+                current = current.left; // Search in the left subtree
+            } else {
+                current = current.right; // Search in the right subtree
+            }
+        }
+
+        return null; // Node with the value 'v' not found
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "value=" + value +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
     }
 }

acd247eecb6017497cc6afc991313372e2ba7fd1
Otto Koridze <okoridze@yahoo.com>
2023-10-25 22:02:10 +0400

    EOL: Lesson 17

--- src/main/java/ge/itstep/demo/CalculatePoints.java
+++ src/main/java/ge/itstep/demo/CalculatePoints.java
@@ -1,56 +1,114 @@
 package ge.itstep.demo;
 
+import ge.itstep.demo.model.ShipType;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class CalculatePoints {
 
+    static boolean occupiedCells[][] = new boolean[10][10];
     public static void main(String[] args) {
 
         final int gridSize = 10;
 
         List<Ship> ships = new ArrayList<>();
 
-
-        Ship ship1 = new Ship(2,2, 5, 2);
-
-//                          [X][Y]
-        boolean occupiedCells[][] = new boolean[gridSize][gridSize];
-
-        if (ship1.isHorizontal()) {
-            for (int x = ship1.getX1(); x <= ship1.getX2(); x++) {
-
-                occupiedCells[x][ship1.getY1()] = true;
-
-                if (ship1.getX1() > 0) {
-                    occupiedCells[x - 1][ship1.getY1()] = true;
-                }
-                if (ship1.getX2() < gridSize) {
-                    occupiedCells[x + 1][ship1.getY1()] = true;
-                }
-                if (ship1.getY1() > 0) {
-                    occupiedCells[x][ship1.getY1() - 1] = true;
-                }
-                if (ship1.getY2() < gridSize) {
-                    occupiedCells[x][ship1.getY2() + 1] = true;
-                }
-
-                if (ship1.getX1() > 0 &&
-                        ship1.getY1() > 0 &&
-                        ship1.getX2() < gridSize &&
-                        ship1.getY2() < gridSize
-                ) {
-                    occupiedCells[x - 1][ship1.getY1() - 1] = true;
-                    occupiedCells[x - 1][ship1.getY1() + 1] = true;
-                    occupiedCells[x + 1][ship1.getY2() - 1] = true;
-                    occupiedCells[x + 1][ship1.getY2() - 1] = true;
-                }
-
-            }
+        for (int s = 0; s < 2; s++)
+        {
+            ships.add(generateShips(gridSize, ShipType.FOUR_SLOT));
+            ships.add(generateShips(gridSize, ShipType.TWO_SLOT));
         }
+
 
         System.out.println(gridToString(occupiedCells));
 
+        System.out.println(ships.toString());
+
+    }
+
+    public static Ship generateShips(int gridSize, ShipType type)
+    {
+        Random random = new Random();
+        Ship ship = new Ship();
+        int lowerLimit = 0, upperLimit = 9, x, y;
+        boolean isHorizontal = random.nextInt() % 2 == 0;
+        if (isHorizontal) {
+            x = random.nextInt(upperLimit - lowerLimit + 1) + lowerLimit;
+            while(x + type.getSize() > gridSize) {
+                x = random.nextInt(upperLimit - lowerLimit + 1) + lowerLimit;
+            }
+            y = random.nextInt(upperLimit - lowerLimit + 1) + lowerLimit;
+
+            while (notInOccupiedArea(x, y, x + type.getSize(), y)) {
+                markHolesAsOccupied(x, y, x + type.getSize(), y);
+                ship.setX1(x);
+                ship.setY1(y);
+                ship.setX2(x + type.getSize());
+                ship.setY2(y);
+            }
+
+
+        } else {
+            x = random.nextInt(upperLimit - lowerLimit + 1) + lowerLimit;
+            y = random.nextInt(upperLimit - lowerLimit + 1) + lowerLimit;
+            while (y + type.getSize() > gridSize) {
+                y = random.nextInt(upperLimit - lowerLimit + 1) + lowerLimit;
+            }
+            while (notInOccupiedArea(x, y, x, y + type.getSize())) {
+                markHolesAsOccupied(x, y, x, y + type.getSize());
+                ship.setX1(x);
+                ship.setY1(y);
+                ship.setX2(x);
+                ship.setY2(y + type.getSize());
+            }
+
+        }
+
+        return ship;
+    }
+
+    public static void markHolesAsOccupied(int x1, int y1, int x2, int y2)
+    {
+        boolean isHorizontal = y1 == y2;
+        if (isHorizontal) {
+            for (int x = x1; x < x2; x++) {
+                findOccupies(x, y1, 10);
+            }
+        } else {
+            for (int y = y1; y < y2; y++) {
+                findOccupies(x1, y, 10);
+            }
+        }
+
+    }
+
+    public static boolean notInOccupiedArea(int x1, int y1, int x2, int y2)
+    {
+        boolean isHorizontal = y1 == y2;
+        if (isHorizontal) {
+            for (int x = x1; x < x2; x++) {
+                if (occupiedCells[x][y1]) return false;
+            }
+        } else {
+            for (int y = y1; y < y2; y++) {
+                if (occupiedCells[x1][y]) return false;
+            }
+        }
+        return true;
+    }
+
+    public static void findOccupies(int x, int y, int gridsize)
+    {
+        if (x - 1 > -1) occupiedCells[x - 1][y] = true;
+        if (x + 1 < gridsize) occupiedCells[x + 1][y] = true;
+        if (y + 1 < gridsize) occupiedCells[x][y + 1] = true;
+        if (y - 1 > -1) occupiedCells[x][y - 1] = true;
+        if (x - 1 > -1 && y - 1 > -1) occupiedCells[x - 1][y - 1] = true;
+        if (x - 1 > -1 && y + 1 < gridsize) occupiedCells[x - 1][y + 1] = true;
+        if (x + 1 < gridsize && y + 1 < gridsize) occupiedCells[x + 1][y + 1] = true;
+        if (x + 1 < gridsize && y - 1 > -1) occupiedCells[x + 1][y - 1] = true;
     }
 
 
@@ -76,13 +134,14 @@ class Ship {
     private int x2;
     private int y2;
 
+
     public Ship() {
     }
 
-    public Ship(int x1, int y1, int endX, int y2) {
+    public Ship(int x1, int y1, int x2, int y2) {
         this.x1 = x1;
         this.y1 = y1;
-        this.x2 = endX;
+        this.x2 = x2;
         this.y2 = y2;
     }
 
@@ -121,5 +180,15 @@ class Ship {
 
     public void setY2(int y2) {
         this.y2 = y2;
+    }
+
+    @Override
+    public String toString() {
+        return "Ship{" +
+                "x1=" + x1 +
+                ", y1=" + y1 +
+                ", x2=" + x2 +
+                ", y2=" + y2 +
+                '}';
     }
 }
--- src/main/java/ge/itstep/demo/service/GameService.java
+++ src/main/java/ge/itstep/demo/service/GameService.java
@@ -16,12 +16,38 @@ public class GameService {
     @Autowired
     private ShipRepository shipRepository;
 
+    private final int gridSize = 10;
+    private boolean occupiedCells[][] = new boolean[gridSize][gridSize];
+
     public Game init(User user, int gridSize)
     {
         Game game = new Game();
         game.setGridSize(gridSize);
         game.setUser(user);
         return gameRepository.save(game);
+    }
+
+    private void generateShips()
+    {
+//        TODO: დავაგენერიროთ თითეული ტიპის გემი 2-2-ჯერ ისე რომ ისინი არ იკვეთებდონენ და განლაგება იყოს ვალიდური
+    }
+
+    public boolean shoot(int x, int y)
+    {
+//        TODO: X და Y კოორდინატზე ვეძებთ გემს და ვესვრით
+        return false;
+    }
+
+    private void findOccupies(int x, int y, int gridsize)
+    {
+        if (x - 1 > -1) this.occupiedCells[x - 1][y] = true;
+        if (x + 1 < gridsize) this.occupiedCells[x + 1][y] = true;
+        if (y + 1 < gridsize) this.occupiedCells[x][y + 1] = true;
+        if (y - 1 > -1) this.occupiedCells[x][y - 1] = true;
+        if (x - 1 > -1 && y - 1 > 1) this.occupiedCells[x - 1][y - 1] = true;
+        if (x - 1 > -1 && y + 1 < gridsize) this.occupiedCells[x - 1][y + 1] = true;
+        if (x + 1 < gridsize && y + 1 < gridsize) this.occupiedCells[x + 1][y + 1] = true;
+        if (x + 1 < gridsize && y - 1 > -1) this.occupiedCells[x + 1][y - 1] = true;
     }
 
 

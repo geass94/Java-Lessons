6946a67d91d1cf10221b1c3f1daad961ec9c1bf1
WYBM <saluqvadze2006@gmail.com>
2023-10-30 22:01:00 +0400

    CW shipformula

--- src/main/java/ge/itstep/demo/ClassWork15.java
+++ src/main/java/ge/itstep/demo/ClassWork15.java
@@ -3,20 +3,56 @@ package ge.itstep.demo;
 public class ClassWork15 {
     public static void main(String[] args) {
 
-        Node root = new Node(10);
+//        Node root = new Node(10);
+//
+//        root.left = new Node(5);
+//        root.right = new Node(15);
+//
+//
+//        root.left.left = new Node(3);
+//        root.left.right = new Node(7);
+//
+//        root.right.left = new Node(12);
+//        root.right.right = new Node(28);
+//
+//        Node result = root.find(15);
+//        System.out.println(result.toString());
 
-        root.left = new Node(5);
-        root.right = new Node(15);
+//        for (int i = 0; i <= 10; i++) {
+//
+//            for (int j = 1; j <= 10 - 1; j++) {
+//                System.out.print(" ");
+//            }
+//            for (int k = i-1; k <= i; k++) {
+//                System.out.print(i);
+//            }
+//            for (int j = (i - 1); j >= 1 ; j--) {
+//                System.out.print(j);
+//
+//            }
+//            System.out.println();
+//
+//        }
+        System.out.println(IsPalindrom("rogor"));
+        //oto //oto //rogor //rogor
 
 
-        root.left.left = new Node(3);
-        root.left.right = new Node(7);
+    }
+    public static boolean IsPalindrom(String str){
+//        StringBuilder sb = new StringBuilder();
+//        sb.append(str);
+//        sb.reverse();
+//        if (str.equals(sb.toString())){
+//            return true;
+//        }
+//        return false;
 
-        root.right.left = new Node(12);
-        root.right.right = new Node(28);
-
-        Node result = root.find(15);
-        System.out.println(result.toString());
+        for (int i = 0; i < (str.length()/2) ; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - i - 1)){
+                return false;
+            }
+        }
+        return true;
     }
 
 
--- src/main/java/ge/itstep/demo/HomeWork18.java
+++ src/main/java/ge/itstep/demo/HomeWork18.java
@@ -3,12 +3,56 @@ package ge.itstep.demo;
 public class HomeWork18 {
     public static void main(String[] args) {
         int[][] data = {
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9}
+                {1, 2, 3}, // 1 4 7
+                {4, 5, 6}, // 2 5 8
+                {7, 8, 9}  // 3 6 9
         };
-        /*
-        * რასაც კითხულობთ ვერტიკალურად ეგ უნდა გარდაქმნათ ახალ მასივად ოღონდ ჰორიზნტებში უნდა იყოს ჩაწერილი ვერტიკალები.
-        * */
+        /* რასაც კითხულობთ ვერტიკალურად ეგ უნდა გარდაქმნათ ახალ
+        * მასივად ოღონდ ჰორიზნტებში უნდა იყოს ჩაწერილი ვერტიკალები. */
+        DrawMatrix(data);
+        int[][] data2 = DrawMatrixHerizontallyVerticalled(data);
+        DrawMatrix(data2);
+
+
+        String[] examplesaba = {"qvabi", "qvaba", "qvavri"}; //qva
+        String[] example1 = {"flower", "flow", "flight"};
+        String[] example2 = {"dog", "racecar", "car"};
+
+
+
+
+    }
+    public static void LongestCommonPrefix(String[] strs){
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) > -1){
+
+            }
+
+
+
+        }
+    }
+    public static void DrawMatrix(int[][] matrix){
+        System.out.println("===================");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix[i][j]  + ",");
+            }
+            System.out.println();
+        };
+        System.out.println("===================");
+    }
+    public static int[][] DrawMatrixHerizontallyVerticalled(int[][] matrixtobereversed){
+        int[][] HorizontallyVerticalledMatrix = new int[matrixtobereversed.length][matrixtobereversed.length];
+        for (int i = 0; i < matrixtobereversed.length; i++) {
+            //System.out.println("in data: " + data[0][i]);
+            for (int j = 0; j < matrixtobereversed[i].length; j++) {
+                HorizontallyVerticalledMatrix[i][j] = matrixtobereversed[j][i];
+                //System.out.println("Added in data2: " + data2[i][j]);
+            }
+        }
+        return HorizontallyVerticalledMatrix;
     }
 }

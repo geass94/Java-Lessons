fcf4a41a41f7dcc37059e513f4373c275b2dc90e
Otto Koridze <okoridze@yahoo.com>
2023-10-30 21:14:49 +0400

    WIP

---
+++ src/main/java/ge/ClassWork19.java
@@ -0,0 +1,43 @@
+package ge;
+
+public class ClassWork19 {
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        list.print();
+    }
+
+}
+
+class LinkedList {
+    Node node;
+
+    public void push(int y) {
+        Node newNode = new Node(y);
+        newNode.next = newNode;
+        node = newNode;
+    }
+
+    public void print() {
+        Node current = node;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
+
+class Node {
+    public int data;
+    public Node next;
+
+    public Node(int x) {
+        this.data = x;
+        this.next = null;
+    }
+}
